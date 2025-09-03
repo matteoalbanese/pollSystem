ab7e6035f632304082f1ef6f1f18753e698dd148
matalban <matteoalbanese17@gmail.com>
2025-09-03 12:01:19 +0200

    Project Created

---
+++ src/main/java/com/matalban/pollsystem/PollSystemApplication.java
@@ -0,0 +1,13 @@
+package com.matalban.pollsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PollSystemApplication.class, args);
+    }
+
+}
